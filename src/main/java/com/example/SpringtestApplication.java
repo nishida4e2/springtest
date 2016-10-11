27f9ec92acd875638e0e6d0164b00855fc1bed01
s_nishida <s_nishida@four-e.co.jp>
2016-10-11 16:23:36 +0900

    hello world test

--- src/main/java/com/example/SpringtestApplication.java
+++ src/main/java/com/example/SpringtestApplication.java
@@ -2,11 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringtestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtestApplication.class, args);
 	}
+	
+    @RequestMapping(value = "/")
+    String hello() {
+        return "Hello World!";
+    }
 }
