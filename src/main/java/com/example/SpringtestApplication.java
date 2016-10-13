e5de415ca14c24b9b88501b2b33a2862f0355800
s_nishida <s_nishida@four-e.co.jp>
2016-10-13 20:35:06 +0900

    webjars

--- src/main/java/com/example/SpringtestApplication.java
+++ src/main/java/com/example/SpringtestApplication.java
@@ -3,7 +3,6 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
@@ -16,11 +15,6 @@ public class SpringtestApplication {
 		SpringApplication.run(SpringtestApplication.class, args);
 	}
 	
-    @RequestMapping(value = "/")
-    String hello() {
-        return "Hello World!";
-    }
-    
     @RequestMapping("/hello")
     public String hello(@RequestParam(value="name", required=false, defaultValue="test") String name, Model model) {
         model.addAttribute("name", name);
