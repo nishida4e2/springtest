995e307eea24cca07ae0bfa4186356242f0f3ca8
s_nishida <s_nishida@four-e.co.jp>
2016-10-14 20:11:15 +0900

    mysql
    create database springtestdb

---
+++ src/main/java/com/example/domain/FoodPrice.java
@@ -0,0 +1,23 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "foodprice")
+public class FoodPrice {
+    @Id
+    @GeneratedValue
+    private Integer id;
+	private String name;
+    private String price;
+}
