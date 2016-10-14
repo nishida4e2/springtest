05173ca80488f0536df5798725e91147e49d5553
s_nishida <s_nishida@four-e.co.jp>
2016-10-14 21:04:12 +0900

    format

--- src/main/java/com/example/web/TestController.java
+++ src/main/java/com/example/web/TestController.java
@@ -7,32 +7,32 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/")
 public class TestController {
-	
+
 	String viewtest;
-	
-    @GetMapping
-    String init(Model model) {
-    	//viewtest = "1";
-    	model.addAttribute("name", viewtest);
-        return "menulist";
-    }
-    
-    @PostMapping(path = "edit")
-    String edit(Model model) {
-    	//model.addAttribute("name", "edit_test");
-    	//return "hello";
-    	viewtest = "2";
-    	return "redirect:/";
-    }
-    
-    @PostMapping(path = "reg")
-    String reg() {
-    	return "reg";
-    }
-    
-    @PostMapping(path = "back")
-    String back() {
-    	return "redirect:/";
-    }
+
+	@GetMapping
+	String init(Model model) {
+		// viewtest = "1";
+		model.addAttribute("name", viewtest);
+		return "menulist";
+	}
+
+	@PostMapping(path = "edit")
+	String edit(Model model) {
+		// model.addAttribute("name", "edit_test");
+		// return "hello";
+		viewtest = "2";
+		return "redirect:/";
+	}
+
+	@PostMapping(path = "reg")
+	String reg() {
+		return "reg";
+	}
+
+	@PostMapping(path = "back")
+	String back() {
+		return "redirect:/";
+	}
 
 }
