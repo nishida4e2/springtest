45a6217d5ed693fa7fa0676ebba6d7f0949211f5
s_nishida <s_nishida@four-e.co.jp>
2016-10-14 19:21:49 +0900

    testcontroller

--- src/main/java/com/example/SpringtestApplication.java
+++ src/main/java/com/example/SpringtestApplication.java
@@ -2,23 +2,12 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@Controller
 public class SpringtestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringtestApplication.class, args);
 	}
 	
-    @RequestMapping("/hello")
-    public String hello(@RequestParam(value="name", required=false, defaultValue="test") String name, Model model) {
-        model.addAttribute("name", name);
-        return "hello";
-    }
-    
 }
---
+++ src/main/java/com/example/web/TestController.java
@@ -0,0 +1,28 @@
+package com.example.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/")
+public class TestController {
+	
+	String viewtest;
+	
+    @GetMapping
+    String init(Model model) {
+    	//viewtest = "1";
+    	model.addAttribute("name", viewtest);
+        return "menulist";
+    }
+    
+    @PostMapping(path = "edit")
+    String edit(Model model) {
+    	//model.addAttribute("name", "edit_test");
+    	//return "hello";
+    	viewtest = "2";
+    	return "redirect:/";
+    }
+
+}
