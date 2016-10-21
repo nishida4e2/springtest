856a9506030ac9557db534e9df3c4c3b83911bb9
nishida4e <s_nishida@four-e.co.jp>
2016-10-21 14:41:06 +0900

    format

--- src/main/java/com/example/web/FoodPriceForm.java
+++ src/main/java/com/example/web/FoodPriceForm.java
@@ -1,17 +1,16 @@
 package com.example.web;
 
 import javax.validation.constraints.Size;
-
 import lombok.Data;
 
 @Data
 public class FoodPriceForm {
 
 	private int id;
-	
-    @Size(min = 1, max = 4, message = "商品名は{min}文字以上{max}文字以内で入力してください")
+
+	@Size(min = 1, max = 4, message = "商品名は{min}文字以上{max}文字以内で入力してください")
 	private String name;
-    
+
 	private int price;
 
 }
